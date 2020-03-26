68139bd22f91349a5a197c9d95b2287a0e9e32cb
Joe Garibaldi <joe@Joes-MBP.lan>
2020-03-25 18:17:21 -0700

    Custom Arraylist+switches

--- src/_01_Custom_ArrayList/ArrayList.java
+++ src/_01_Custom_ArrayList/ArrayList.java
@@ -2,34 +2,70 @@ package _01_Custom_ArrayList;
 
 @SuppressWarnings("unchecked")
 
-public class ArrayList <T>{
-	
+public class ArrayList<T> {
+	T[] items = (T[]) new Object[0];
+
 	public ArrayList() {
+
 	}
-	
+
 	public T get(int loc) throws IndexOutOfBoundsException {
-		
-		return null;
+
+		return items[loc];
 	}
-	
+
 	public void add(T val) {
-		
+		T[] update = (T[]) new Object[items.length + 1];
+		for (int i = 0; i < items.length; i++) {
+			update[i] = items[i];
+		}
+		update[items.length] = val;
+		items = update;
 	}
-	
+
 	public void insert(int loc, T val) throws IndexOutOfBoundsException {
-		
+		T[] update = (T[]) new Object[items.length + 1];
+				for (int i = 0; i < update.length; i++) {
+			if (i < loc) {
+				update[i] = items[i];
+			} else if (i > loc) {
+				
+				update[i] = items[i - 1];
+			}else {
+				update[i]=val;
+			}
+		}
+		items = update;
 	}
-	
+
 	public void set(int loc, T val) throws IndexOutOfBoundsException {
-		
+		items[loc] = val;
 	}
-	
+
 	public void remove(int loc) throws IndexOutOfBoundsException {
-		
-	}
+		T[] update = (T[]) new Object[items.length - 1];
+		for (int i = 0; i < update.length; i++) {
+			if (i >= loc) {
+				update[i] = items[i + 1];
+			}
+			if (i < loc) {
+				update[i] = items[i];
+			}
+		}
+		items = update;
 	
+	}
+
 	public boolean contains(T val) {
-		
+		for (int i = 0; i < items.length; i++) {
+			if (items[i] == val) {
+				return true;
+			}
+		}
 		return false;
+	}
+
+	public int size() {
+		return items.length;
 	}
 }
--- src/_03_Switch_Statement_Practice/CustomButtonOptionPanes.java
+++ src/_03_Switch_Statement_Practice/CustomButtonOptionPanes.java
@@ -12,6 +12,28 @@ public class CustomButtonOptionPanes {
 		String choice = options[input];
 		
 		//use a switch statement to do something cool for each option
-		
-	}
+		switch(choice) {
+		case "Sunday": 
+			System.out.println("Sorry, but the churches are closed due to COVID19. I hope God understands.");
+			break;
+		case "Monday": 
+			System.out.println("Don't mention today's name to Garfield. He might go beserk.");
+			break;
+		case "Tuesday": 
+			System.out.println("Isn't it weird to not physically be at school at this poiint in the week?");
+			break;
+		case "Wedneday": 
+			System.out.println("By now youv'e probably adjusted to the new scedules.");
+			break;
+		case "Thursday": 
+			System.out.println("You ate five snickers bars during class today. Probably because your desk is next to the pantry now.");
+			break;
+		case "Friday": 
+			System.out.println("In a normal school week, you'd rejoice at the coming of the weekend. But your still home all day, so there is very little difference.");
+			break;
+		case "Saturday":
+			System.out.println("Somehow, Saturday no longer feels mystical. You don't have to go to school, but you weren't all week anyways.");
+			break;
+		}
+		}
 }
